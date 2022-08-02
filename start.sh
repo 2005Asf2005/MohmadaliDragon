66e2476e954cf4ab235ba22cdbc6f5d351e1eb34
Akieno-sourse <108626259+Akieno-sourse@users.noreply.github.com>
2022-08-02 17:37:32 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/RR9R7/Youtube.git /AnyDLBot
+git clone https://github.com/Akieno-sourse/MohmadaliDragon.git /AnyDLBot
 cd /Youtube
 pip3 install -r requirements.txt
 echo "Starting Bot...."
